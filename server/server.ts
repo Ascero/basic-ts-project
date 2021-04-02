43bf3bb1827e2297fffa7268f775ef137c2dfe25
Daniel Vieira <danvie11@gmail.com>
2021-04-02 22:28:05 +0200

    TSNP-0001 feat(app) npm install --save-dev eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser for linting

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ const port: string | number = process.env.port || 1337;
 const app = express();
 
 app.listen(port);
-debugger;
+
 app.use(express.static('src'));
 
 console.log('Listen to port ' + port);
